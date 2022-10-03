d862ad33ee21062b32ca54cda0d27ad073a3d979
nevadimka <myregbox@outlook.com>
2022-10-03 22:46:15 +0300

    frontend base

---
+++ frontend/src/components/App.js
@@ -0,0 +1,11 @@
+import React, { Fragment } from "react";
+
+function App() {
+    return (
+        <Fragment>
+            Hello Billy
+        </Fragment>
+    );
+}
+
+export default App;
---
+++ frontend/webpack.config.js
@@ -0,0 +1,31 @@
+const path = require('path');
+const HtmlWebpackPlugin = require("html-webpack-plugin");
+const { plugins } = require('@babel/preset-env/lib/plugins-compat-data');
+
+module.exports = {
+    entry: "./src/index.js",
+    output: {
+        path: path.join(__dirname, "/dist"),
+        filename: "bundle.js"
+    },
+    module: {
+        rules: [
+            {
+                test: /\.js$/,
+                exclude: '/node_modules/',
+                use: {
+                    loader: "babel-loader"
+                }
+            },
+            {
+                test: /\.css$/,
+                use: ["style-loader", "css-loader"]
+            }
+        ]
+    },
+    plugins: [
+        new HtmlWebpackPlugin({
+            template: "./src/index.html"
+        })
+    ]
+}
